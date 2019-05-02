4e2c32de6a83cfc9367898c9bd807f4194b8f200
meirusfandi <meirusfandi100@gmail.com>
2019-05-02 16:56:19 +0700

    üpdate

--- database/database.go
+++ database/database.go
@@ -1,62 +1,72 @@
 package database
 
 import (
+	"database/sql"
+	"fmt"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// type User struct {
-// 	id       int    `json:"id"`
-// 	username string `json:"username"`
-// 	password string `json:"password"`
-// 	email    string `json:"email"`
-// 	name     string `json:"name"`
-// }
+type User struct {
+	// id       int    `json:"id"`
+	// username string `json:"username"`
+	// password string `json:"password"`
+	// email    string `json:"email"`
+	// name     string `json:"name"`
 
-// func ConnectDB() (*sql.DB, error) {
-// 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:8080)/golang")
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	id       int
+	username string
+	password string
+	email    string
+	name     string
+}
 
-// 	return db, nil
-// }
+func ConnectDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:8080)/golang")
+	if err != nil {
+		return nil
+	}
 
-func GetAllUser() {
+	return db
+}
+
+func GetAllUser() []User {
 	//get data from database
-	// db, err := ConnectDB()
+	db := ConnectDB()
+	fmt.Println("connecting to database has been successfully")
 
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
-	// defer db.Close()
+	data, err := db.Query("select * from pengguna")
+	if err != nil {
+		fmt.Println(err.Error())
+		checkErr(err)
+	}
+	fmt.Println("get data from database has been successfully")
 
-	// rows, err := db.Query("select * from pengguna")
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
-	// defer rows.Close()
+	var result []User
 
-	// var result []User
+	for data.Next() {
+		var each = User{}
+		var err = data.Scan(&each.id, &each.username, &each.password, &each.name, &each.email)
+		if err != nil {
+			fmt.Println(err.Error())
+			checkErr(err)
+		}
 
-	// for rows.Next() {
-	// 	var each = User{}
-	// 	var err = rows.Scan(&each.id, &each.username, &each.password, &each.name, &each.email)
-	// 	if err != nil {
-	// 		fmt.Println(err.Error())
-	// 		return
-	// 	}
+		result = append(result, each)
+	}
 
-	// 	result = append(result, each)
-	// }
+	if err = data.Err(); err != nil {
+		fmt.Println(err.Error())
+		checkErr(err)
+	}
 
-	// if err = rows.Err(); err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
+	fmt.Println(result)
 
-	// fmt.Println(result)
+	return result
+}
 
-	// return result
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
