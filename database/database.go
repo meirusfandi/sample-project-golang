84fd5a80449c4bbd3f7678f818692e91c1a377cf
meirusfandi <meirusfandi100@gmail.com>
2019-05-02 15:32:11 +0700

    update database

--- database/database.go
+++ database/database.go
@@ -1,61 +1,62 @@
 package database
 
 import (
-	"database/sql"
-	"fmt"
-
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type User struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
-	Name     string `json:"name"`
-}
+// type User struct {
+// 	id       int    `json:"id"`
+// 	username string `json:"username"`
+// 	password string `json:"password"`
+// 	email    string `json:"email"`
+// 	name     string `json:"name"`
+// }
 
-func ConnectDB() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:8080)/golang")
-	if err != nil {
-		return nil, err
-	}
+// func ConnectDB() (*sql.DB, error) {
+// 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:8080)/golang")
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return db, nil
-}
+// 	return db, nil
+// }
 
 func GetAllUser() {
 	//get data from database
-	db, err := ConnectDB()
+	// db, err := ConnectDB()
 
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer db.Close()
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// defer db.Close()
 
-	rows, err := db.Query("select * from pengguna")
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer rows.Close()
+	// rows, err := db.Query("select * from pengguna")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	// defer rows.Close()
 
-	var result []User
+	// var result []User
 
-	for rows.Next() {
-		var each = User{}
-		var err = rows.Scan(&each.ID, &each.Username, &each.Password, &each.Name, &each.Email)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
+	// for rows.Next() {
+	// 	var each = User{}
+	// 	var err = rows.Scan(&each.id, &each.username, &each.password, &each.name, &each.email)
+	// 	if err != nil {
+	// 		fmt.Println(err.Error())
+	// 		return
+	// 	}
 
-		result = append(result, each)
-	}
+	// 	result = append(result, each)
+	// }
 
-	if err = rows.Err(); err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	// if err = rows.Err(); err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+
+	// fmt.Println(result)
+
+	// return result
 }
--- handler/handler.go
+++ handler/handler.go
@@ -1,24 +1,89 @@
 package handler
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
 	"path"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func IndexUserPage(w http.ResponseWriter, r *http.Request) {
-	file := path.Join("view", "index.html")
-	var templates, erro = template.ParseFiles(file)
-	if erro != nil {
-		http.Error(w, erro.Error(), http.StatusInternalServerError)
-		return
+type User struct {
+	id       int    `json:"id"`
+	username string `json:"username"`
+	password string `json:"password"`
+	email    string `json:"email"`
+	fullname string `json:"fullname"`
+}
+
+func ConnectDB() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:8080)/golang")
+	if err != nil {
+		return nil, err
 	}
 
-	erro = templates.Execute(w, nil)
-	if erro != nil {
-		http.Error(w, erro.Error(), http.StatusInternalServerError)
+	return db, nil
+}
+
+//index for get all user on one page
+func IndexUserPage(w http.ResponseWriter, r *http.Request) {
+	//get data from database
+	// var result = database.GetAllUser()
+
+	db, err := ConnectDB()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
+	defer db.Close()
+
+	fmt.Println("connect db is successfully")
+
+	rows, err := db.Query("SELECT * FROM pengguna")
+	if err != nil {
+		fmt.Println("select data is failed")
+		fmt.Println(err.Error())
+		return
+	}
+	defer rows.Close()
+
+	fmt.Println("select data is successfully")
+
+	// var result []User
+
+	// for rows.Next() {
+	// 	var each = User{}
+	// 	var err = rows.Scan(&each.id, &each.username, &each.password, &each.fullname, &each.email)
+	// 	if err != nil {
+	// 		fmt.Println(err.Error())
+	// 		return
+	// 	}
+
+	// 	result = append(result, each)
+	// }
+
+	// if err = rows.Err(); err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+
+	// fmt.Println(result)
+
+	// file := path.Join("view", "index.html")
+	// var templates, erro = template.ParseFiles(file)
+
+	// if erro != nil {
+	// 	http.Error(w, erro.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// erro = templates.Execute(w, result)
+	// if erro != nil {
+	// 	http.Error(w, erro.Error(), http.StatusInternalServerError)
+	// }
 }
 
 func IndexOneUserPage(w http.ResponseWriter, r *http.Request) {
